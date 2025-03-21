30ad39083b3432e745ff359eca2df8e535c08fc8
Umatambo Ariel <victorarielcr7rm@gmail.com>
2025-03-20 23:14:20 -0500

    MVC tablas: Student, Administrative, Course

---
+++ model/administrativeModel.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Administrative = sequelize.define('Administrative', {
+    id_administrative: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true }
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    role: {
+        type: DataTypes.ENUM('administrator', 'professor', 'student', 'parent'),
+        allowNull: false
+    }
+}, {
+    tableName: 'administratives',
+    timestamps: false
+});
+
+module.exports = Administrative;
